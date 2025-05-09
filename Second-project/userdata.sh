30d050d549314087f11ed954d167dcce08493595
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-09 15:13:48 -0400

    Update userdata.sh

--- Second-project/userdata.sh
+++ Second-project/userdata.sh
@@ -5,20 +5,20 @@ exec > /var/log/userdata.log 2>&1
 set -x
 
 # Update and install packages
-apt-get update -y
-apt-get install -y apache2 awscli
+sudo apt-get update -y
+sudo apt-get install -y apache2 awscli
 
 # Get the instance ID
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 # Install the AWS CLI
-apt install -y awscli
+sudo apt install -y awscli
 
 # Download the images from S3 bucket
 #aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
 
 # Create the directory for the application
-mkdir -p /var/www/html/app1
+sudo mkdir -p /var/www/html/app1
 
 # Create HTML content
 cat <<EOF > /var/www/html/app1/index.html
@@ -46,5 +46,5 @@ cat <<EOF > /var/www/html/app1/index.html
 EOF
 
 # Start and enable Apache
-systemctl start apache2
-systemctl enable apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
