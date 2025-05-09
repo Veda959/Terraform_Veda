c7b738b4283209bdd1804c72c5eed44e5c25ed6a
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-09 13:00:15 -0400

    Update main.tf

--- Second-project/main.tf
+++ Second-project/main.tf
@@ -50,6 +50,15 @@ resource "aws_security_group" "webSg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description = "SSH"
     from_port   = 22
