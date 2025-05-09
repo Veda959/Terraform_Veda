a05330432a8dac7f0443786fd1cb33590fdf477e
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-09 12:27:16 -0400

    Update main.tf

--- Second-project/main.tf
+++ Second-project/main.tf
@@ -78,6 +78,7 @@ resource "aws_s3_bucket" "example" {
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.micro"
+  key_name               = "terraform-key"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
   user_data              = base64encode(file("userdata.sh"))
@@ -86,6 +87,7 @@ resource "aws_instance" "webserver1" {
 resource "aws_instance" "webserver2" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.micro"
+  key_name               = "terraform-key"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
   user_data              = base64encode(file("userdata1.sh"))
