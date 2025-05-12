b3be017ba5a33a0bc0530e347142b0d9312b1ad0
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 09:56:29 -0400

    Update outputs.tf

--- EC2_and_Jenkins_Launch/outputs.tf
+++ EC2_and_Jenkins_Launch/outputs.tf
@@ -1,1 +1,32 @@
+output "vpc_id" {
+  value = aws_vpc.jenkins_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.jenkins_subnet.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.jenkins_igw.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.jenkins_route_table.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.jenkins_sg.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.jenkins_ec2.public_ip
+}
+
+output "jenkins_url" {
+  value = "http://${aws_instance.jenkins_ec2.public_ip}:8080"
+}
+
+output "ssh_access" {
+  value = "ssh -i ~/.ssh/terraform-key.pem ubuntu@${aws_instance.jenkins_ec2.public_ip}"
+}
 
