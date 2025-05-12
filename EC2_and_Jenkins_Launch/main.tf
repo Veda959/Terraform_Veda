c08f83de500a00a52a973779eff4733924429fe2
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 15:29:44 -0400

    Update main.tf

--- EC2_and_Jenkins_Launch/main.tf
+++ EC2_and_Jenkins_Launch/main.tf
@@ -40,7 +40,7 @@ resource "aws_route_table_association" "main" {
   route_table_id = aws_route_table.main.id
 }
 
-# Create Security Group
+# Create Master instance Security Group
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -66,6 +66,28 @@ resource "aws_security_group" "main" {
   }
 }
 
+# Create a Slave instance Security Group
+resource "aws_security_group" "slave_sg" {
+  name        = "slave-sg"
+  description = "Allow SSH only from Jenkins_Master SG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.main.id]
+    description     = "SSH from Jenkins_Master SG"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Create an EC2 instance
 resource "aws_instance" "main" {
   ami           = var.ami_id          
@@ -75,7 +97,20 @@ resource "aws_instance" "main" {
   security_groups = [aws_security_group.main.id]
 
   tags = {
-    Name = "MyTerraformEC2"
+    Name = "Jenkins_Master"
   }
 }
 
+# Create another EC2 instance
+
+resource "aws_instance" "main2" {
+  ami           = var.ami_id          
+  instance_type = var.instance_type   
+  key_name      = var.key_name        
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.slave_sg.id]
+
+  tags = {
+    Name = "Jenkins-Slave"
+  }
+}
