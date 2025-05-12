7e5769ad89977f91b9631e3e6c041c1d9c6812f3
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 11:43:33 -0400

    Update variables.tf

--- EC2_and_Jenkins_Launch/variables.tf
+++ EC2_and_Jenkins_Launch/variables.tf
@@ -3,10 +3,16 @@ variable "aws_region" {
 }
 
 variable "ami_id" {
-  type = "String"
+  type = string
   default = "ami-0f88e80871fd81e91"
 }
 
 variable "key_name" {
   default = "terraform-key"
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
