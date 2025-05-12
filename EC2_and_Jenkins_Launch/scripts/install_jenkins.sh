a64d17d7056e3af037524b427ac432e2e568cc85
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 10:40:48 -0400

    Update install_jenkins.sh

--- EC2_and_Jenkins_Launch/scripts/install_jenkins.sh
+++ EC2_and_Jenkins_Launch/scripts/install_jenkins.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Update the system
+# Update system
 sudo yum update -y
 
-# Install Java (Amazon Corretto 11 is supported for Jenkins)
+# Install Java 11 (required for Jenkins)
 sudo amazon-linux-extras enable corretto11
 sudo yum install -y java-11-amazon-corretto
 
-# Add the Jenkins repo
-sudo curl --silent --location https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key | sudo tee /etc/pki/rpm-gpg/jenkins.io.key
+# Add Jenkins repo (with correct GPG key)
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo curl --silent --location -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 # Install Jenkins
 sudo yum install -y jenkins
 
-# Start and enable Jenkins
-sudo systemctl daemon-reexec
+# Enable and start Jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
