4273ec83b7eebbe335e1ef7876557917cb7cf821
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 11:16:49 -0400

    Update main.tf

--- EC2_and_Jenkins_Launch/main.tf
+++ EC2_and_Jenkins_Launch/main.tf
@@ -1,57 +1,55 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
-# VPC
-resource "aws_vpc" "jenkins_vpc" {
+# Create VPC
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "jenkins-vpc"
-  }
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
-# Subnet
-resource "aws_subnet" "jenkins_subnet" {
-  vpc_id            = aws_vpc.jenkins_vpc.id
-  cidr_block        = "10.0.1.0/24"
+# Create subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "jenkins-subnet"
-  }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "jenkins_igw" {
-  vpc_id = aws_vpc.jenkins_vpc.id
-  tags = {
-    Name = "jenkins-igw"
-  }
+# Create Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 }
 
-# Route Table
-resource "aws_route_table" "jenkins_route_table" {
-  vpc_id = aws_vpc.jenkins_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.jenkins_igw.id
-  }
-  tags = {
-    Name = "jenkins-rt"
-  }
+# Create Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
 }
 
-# Route Table Association
-resource "aws_route_table_association" "jenkins_rt_assoc" {
-  subnet_id      = aws_subnet.jenkins_subnet.id
-  route_table_id = aws_route_table.jenkins_route_table.id
+# Create a route to the internet
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-# Security Group
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Allow SSH and Jenkins"
-  vpc_id      = aws_vpc.jenkins_vpc.id
+# Associate the route table with the subnet
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create Security Group
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 22
@@ -66,31 +64,17 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "jenkins-sg"
-  }
 }
 
-# EC2 Instance
-resource "aws_instance" "jenkins_ec2" {
-  ami                    = var.ami_id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.jenkins_subnet.id
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  associate_public_ip_address = true
-  user_data              = base64encode(file("install_jenkins.sh"))
+# Create EC2 Instance
+resource "aws_instance" "main" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.main.name]
 
   tags = {
-    Name = "JenkinsServer"
+    Name = "MyTerraformEC2"
   }
 }
-
