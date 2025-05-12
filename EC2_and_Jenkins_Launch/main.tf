9eb999825472de50f7936319fd284e30db8b57a9
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-12 09:55:47 -0400

    Update main.tf

--- EC2_and_Jenkins_Launch/main.tf
+++ EC2_and_Jenkins_Launch/main.tf
@@ -1,1 +1,96 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# VPC
+resource "aws_vpc" "jenkins_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "jenkins-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "jenkins_subnet" {
+  vpc_id            = aws_vpc.jenkins_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "jenkins_igw" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+  tags = {
+    Name = "jenkins-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "jenkins_route_table" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+  }
+  tags = {
+    Name = "jenkins-rt"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "jenkins_rt_assoc" {
+  subnet_id      = aws_subnet.jenkins_subnet.id
+  route_table_id = aws_route_table.jenkins_route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and Jenkins"
+  vpc_id      = aws_vpc.jenkins_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "jenkins_ec2" {
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.jenkins_subnet.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  associate_public_ip_address = true
+  user_data              = file("${path.module}/scripts/install_jenkins.sh")
+
+  tags = {
+    Name = "JenkinsServer"
+  }
+}
 
