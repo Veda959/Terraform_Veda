f78672c2d8fe1fc749c2665b3dc27467fbe46be0
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-21 17:03:59 -0400

    Update variables.tf

--- Lambda_Function_S3/variables.tf
+++ Lambda_Function_S3/variables.tf
@@ -1,17 +1,23 @@
-variable "region" {
-  default = "us-east-1"
-}
-
 variable "source_bucket_name" {
-  default = "devops-user-source-bucket-2025"
-"
+  description = "Source S3 bucket name"
+  type        = string
+  default     = "devops-user-source-bucket-2025"
 }
 
 variable "destination_bucket_name" {
-  default = "devops-user-destination-bucket-2025"
-"
+  description = "Destination S3 bucket name"
+  type        = string
+  default     = "devops-user-destination-bucket-2025"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "lambda_function_name" {
-  default = "copy_txt_lambda"
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "copy_txt_lambda"
 }
