d105226f2d364119755b3f74aae5ab25123bbf66
Veda959 <vkommin1@students.kennesaw.edu>
2025-05-21 17:00:57 -0400

    Update variables.tf

--- Lambda_Function_S3/variables.tf
+++ Lambda_Function_S3/variables.tf
@@ -3,12 +3,12 @@ variable "region" {
 }
 
 variable "source_bucket_name" {
-  default = "default = "devops-user-source-bucket-2025"
+  default = "devops-user-source-bucket-2025"
 "
 }
 
 variable "destination_bucket_name" {
-  default = "default = "devops-user-destination-bucket-2025"
+  default = "devops-user-destination-bucket-2025"
 "
 }
 
